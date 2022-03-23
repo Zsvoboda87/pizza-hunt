c033f642943dd32d88103e032b45757d0a22d622
Zsvob <svobodazachary@gmail.com>
2022-03-23 09:56:35 -0400

    pizza list

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,42 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
     },
     createdBy: {
-      type: String
+      type: String,
     },
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
       type: String,
-      default: 'Large'
+      default: "Large",
     },
     toppings: [],
     comments: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Comment'
-      }
-    ]
+        ref: "Comment",
+      },
+    ],
   },
   {
     toJSON: {
       virtuals: true,
+      getters: true
     },
-    id: false
+    id: false,
   }
 );
 
-  // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+// create the Pizza model using the PizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
+PizzaSchema.virtual("commentCount").get(function () {
   return this.comments.length;
 });
 
